e9dcb0d8c9db5724433b951157fecb34dd577dcd
Davide Ori <davide.ori87@gmail.com>
2020-05-20 15:33:49 +0200

    added license statements here and there

--- cython/ssrga_module.pyx
+++ cython/ssrga_module.pyx
@@ -5,6 +5,24 @@ cimport numpy as np  # needed for function arguments
 import numpy as np  # needed for np.ones, np.empty_like ...
 cimport ssrgaLib
 
+"""
+Copyright (C) 2020 Davide Ori 
+University of Cologne
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+"""
+
 ctypedef np.float32_t float_t
 ctypedef np.float64_t double_t
 ctypedef np.int32_t int_t
