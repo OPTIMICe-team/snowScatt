9210e4ebcee51e9c0c9c732c8fa2862674ab3afe
Davide Ori <davide.ori87@gmail.com>
2021-01-21 15:04:24 +0100

    updated Fig1 with logscale

--- examples/GMD_paper_2021/Fig1_ssrga_beta_gamma_asymptote.py
+++ examples/GMD_paper_2021/Fig1_ssrga_beta_gamma_asymptote.py
@@ -12,6 +12,7 @@ It does not require specific input data
 
 import pandas as pd
 import matplotlib.pyplot as plt
+from matplotlib.ticker import FormatStrFormatter
 
 path = '../../python/snowProperties/'
 particle = path + 'ssrga_coeffs_dendrite.csv'
@@ -22,13 +23,21 @@ axt = ax.twinx()
 axt.scatter(table.index*1.0e3, table.gamma, c='r')
 ax.set_xlabel('Dmax   [mm]')
 ax.set_ylabel(r'$\beta$')
+ax.set_yscale('log')
+ax.set_ylim(0.1,10)
+axt.set_yscale('log')
+axt.set_ylim(1, 4)
 axt.set_ylabel(r'$\gamma$', color='red')
 ax.grid()
-axt.grid(color='red', alpha=0.3)
+ax.yaxis.set_major_formatter(FormatStrFormatter('%d'))
+axt.grid(color='red', alpha=0.3, which='both')
 axt.spines['right'].set_color('red')
-axt.tick_params(axis='y', colors='red')
+axt.tick_params(axis='y', colors='red', which='both')
+axt.yaxis.set_minor_formatter(FormatStrFormatter('%d'))
+axt.yaxis.set_major_formatter(FormatStrFormatter('%d'))
 axt.yaxis.label.set_color('red')
 name = particle.split('/')[-1][6:-4]
 ax.set_title(r'SSRGA $\beta,\gamma$  for '+ name[7:] + ' aggregates')
+fig.tight_layout()
 fig.savefig(name + '.png', dpi=300)
 fig.savefig(name + '.pdf', dpi=300)
